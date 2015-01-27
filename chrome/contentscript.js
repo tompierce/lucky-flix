fb4ed81706b97f13d4dcab4e404f9ada66132451
Tom Pierce <tom.pierce0@gmail.com>
2015-01-27 17:26:23 +0000

    fixed a bug in chrome extension where jquery.popupoverlay would not be loaded before it was used. Also upped the version for submission to Chrome and Firefox stores.

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,14 +1,20 @@
 function afterPageLoad() {
-	loadScript('jquery-1.11.1.min.js');
-	loadScript('jquery.popupoverlay.js');
-	loadScript('script.js');
+
+	// synchronously load scripts
+	loadScript('jquery-1.11.1.min.js', function() {
+		loadScript('jquery.popupoverlay.js', function() {
+			loadScript('script.js');
+		});
+	});
+	
 }
 
-function loadScript(name) {
+function loadScript(name, callback) {
 	var s = document.createElement('script');
 	s.src = chrome.extension.getURL(name);
 	s.onload = function() {
 		this.parentNode.removeChild(this);
+		if (callback) callback();
 	};
 	(document.head || document.documentElement).appendChild(s);
 }
